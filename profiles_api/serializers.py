bd5becb82f1c337ca884eb1e9db185aa8303305b
Rana Faraz Ahmed <ranafarazahmed@gmail.com>
2020-11-05 14:12:36 +0500

    Added POST PUT PATCH and DELETE Methods to our API View

---
+++ profiles_api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """Serializers a name field for tesing our APIView"""
+    name = serializers.CharField(max_length=10)
